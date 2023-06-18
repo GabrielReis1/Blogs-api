002b122bac044736e222a833ea68e878cbe1b3fd
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-06-18 10:04:17 -0300

    requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true
+  });
+
+  return Category;
+};
